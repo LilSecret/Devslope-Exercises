7976bae5a5c44ad5bd1af41fea00a64820d04891
Charles <chuckm248@gmail.com>
2023-03-18 09:51:01 -0600

    fix devslopes javascript dugging part 1

--- javascript/index.js
+++ javascript/index.js
@@ -39,8 +39,30 @@
 
 
 // PART 2
-let userInputCelsius = "100";
-let degreesK = Number(userInputCelsius) + 273.15;
+// let userInputCelsius = "100";
+// let degreesK = Number(userInputCelsius) + 273.15;
 
-console.log('Degrees Kelvin:', degreesK); // Expected: 373.15
-console.log(typeof degreesK);
+// console.log('Degrees Kelvin:', degreesK); // Expected: 373.15
+// console.log(typeof degreesK);
+
+// ###############################Spacer########################
+
+/*
+Directions:
+1. Fix syntax errors.
+2. Run the following code as-is and read the error message.
+3. Fix the mistake, and then re-run the code to check it.
+Expected Output:
+=> WARNING: Insufficient energy!
+*/
+
+let readyToSkate = false;
+let energyLevel = 18000;
+
+if (energyLevel >= 24000) {
+   console.log("Energy level is a go!");
+   readyToSkate = true;
+} else {
+   console.log("WARNING: Insufficient energy!");
+   readyToSkate = false;
+}
