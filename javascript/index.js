fa4ea982895ad7cba629fedc25257a4c4c0c2a3c
Charles <chuckm248@gmail.com>
2023-03-18 09:54:30 -0600

    fix devslopes javascript dugging part 2

--- javascript/index.js
+++ javascript/index.js
@@ -56,13 +56,50 @@ Expected Output:
 => WARNING: Insufficient energy!
 */
 
-let readyToSkate = false;
-let energyLevel = 18000;
+// let readyToSkate = false;
+// let energyLevel = 18000;
 
-if (energyLevel >= 24000) {
-   console.log("Energy level is a go!");
+// if (energyLevel >= 24000) {
+//    console.log("Energy level is a go!");
+//    readyToSkate = true;
+// } else {
+//    console.log("WARNING: Insufficient energy!");
+//    readyToSkate = false;
+// }
+
+/*
+Directions:
+1. The block of code has a few syntax errors.
+2. Run the code as-is to find the mistakes.
+💡 Tip:
+Only one error will be flagged at a time.
+Fix that ONE problem, and then re-run the code to check your work.
+Avoid trying to fix multiple issues at once.
+Expected Output:
+=> Crew & cameraBattery is a go!
+=> Grab your skateboard...
+=> Ready...
+=> Set...
+=> SKATE!
+*/
+
+let readyToSkate = false;
+let crewStatus = true;
+let cameraBattery = "green";
+
+if (crewStatus && cameraBattery === "green") {
+   console.log("Crew & cameraBattery is a go!");
    readyToSkate = true;
 } else {
-   console.log("WARNING: Insufficient energy!");
+   console.log("WARNING: Crew or cameraBattery not ready!");
    readyToSkate = false;
+}
+
+if (readyToSkate) {
+   console.log("Grab your skateboard...");
+   console.log("Ready...");
+   console.log("Set...");
+   console.log("SKATE!");
+} else {
+   console.log("No skating today.");
 }
