f73786fc6eea588ee84233cf0ffeaa0d7a2db24e
Charles <chuckm248@gmail.com>
2023-03-18 09:47:41 -0600

    fix the debugging js part two

--- javascript/index.js
+++ javascript/index.js
@@ -12,25 +12,35 @@
 //   console.log('You are not in this universe');
 // }
 
+// ###############################Spacer########################
+
 // PART 1
-const users = [
-  { name: "Bobby", hobby: "Skateboarding" },
-  { name: "Kyle", hobby: "Music" },
-  { name: "Sarah", hobby: "Hang-gliding" },
-  { name: "Jose", hobby: "Snowboarding" },
-  { name: "Phillip" },
-  { name: "Charles", hobby: "Rock Climbing" },
-  { name: "Tabatha", hobby: "Art" },
-];
+// const users = [
+//   { name: "Bobby", hobby: "Skateboarding" },
+//   { name: "Kyle", hobby: "Music" },
+//   { name: "Sarah", hobby: "Hang-gliding" },
+//   { name: "Jose", hobby: "Snowboarding" },
+//   { name: "Phillip" },
+//   { name: "Charles", hobby: "Rock Climbing" },
+//   { name: "Tabatha", hobby: "Art" },
+// ];
 
-function logUser(user) {
-  if(!user.hobby) {
-    console.log(`Hi, ${user.name}.`);
-  } else {
-    console.log(`Hi, ${user.name}. I'm stoked you enjoy ${user.hobby}`);
-  }
-}
+// function logUser(user) {
+//   if(!user.hobby) {
+//     console.log(`Hi, ${user.name}.`);
+//   } else {
+//     console.log(`Hi, ${user.name}. I'm stoked you enjoy ${user.hobby}`);
+//   }
+// }
 
-users.forEach(user => {
-  logUser(user);
-});
+// users.forEach(user => {
+//   logUser(user);
+// });
+
+
+// PART 2
+let userInputCelsius = "100";
+let degreesK = Number(userInputCelsius) + 273.15;
+
+console.log('Degrees Kelvin:', degreesK); // Expected: 373.15
+console.log(typeof degreesK);
