46f5a6372c6d15de79e7efa7692bf68f951ecc01
Charles <chuckm248@gmail.com>
2023-08-21 16:47:27 -0600

    set active state on one at a time and change theme of page

--- js-website/js/main.js
+++ js-website/js/main.js
@@ -1,9 +1,60 @@
+const theme = 'theme';
+const dataTheme = 'data-theme';
+const themeTab = '.theme-tab';
+const switcherBtn = '.theme-btn';
+const dark = 'dark';
+const light = 'light';
+const open = 'open';
+const active = 'active';
+
 const modalOpen = '[data-open]';
 const modalClose = '[data-close]';
 const isVisible = 'is-visible';
 
+const root = document.documentElement;
+
+const toggleTheme = document.querySelector(themeTab);
+const switcher = document.querySelectorAll(switcherBtn);
+const currentTheme = localStorage.getItem(theme);
+
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
+
+const setActive = (elm, selector) => {
+  if(document.querySelector(`${selector}.${active}`) !== null) {
+    document.querySelector(`${selector}.${active}`).classList.remove(`${active}`);
+  }
+  elm.classList.add(active);
+}
+
+const setTheme = (val) => {
+  if (val === dark) {
+    root.setAttribute(dataTheme, dark);
+    localStorage.setItem(theme, dark);
+  } else {
+    root.setAttribute(dataTheme, light);
+    localStorage.setItem(theme, light);
+  }
+  console.log(localStorage);
+}
+
+toggleTheme.addEventListener('click', function() {
+  const parent = this.parentElement;
+
+  if(!parent.className.includes(open)) {
+    parent.classList.add(open);
+  } else {
+    parent.classList.remove(open);
+  }
+})
+
+for (let elm of switcher) {
+  elm.addEventListener('click', function() {
+    const toggle = this.dataset.toggle;
+    setActive(elm, switcherBtn);
+    setTheme(toggle);
+  })
+}
 
 for (const elm of openModal) {
   elm.addEventListener('click', function() {
